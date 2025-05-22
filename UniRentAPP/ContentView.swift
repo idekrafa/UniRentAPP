c6c1eb63ae80caf737e9205aaac190459264e736
Rafael Ribeiro <rafaelribeiro2004@icloud.com>
2025-05-22 18:21:31 -0300

    Changed pfp border shadow

--- UniRentAPP/ContentView.swift
+++ UniRentAPP/ContentView.swift
@@ -73,7 +73,7 @@ struct AppBarView: View {
                 .frame(width: 50, height: 50)
                 .clipShape(Circle())
                 .overlay(Circle().stroke(Color.white, lineWidth: 2))
-                .shadow(radius: 5)
+                .shadow(radius: 2)
         }
         .padding(.horizontal)
     }
