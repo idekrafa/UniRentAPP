b767ba6a1b40e0de25e61909f11c401d3372400b
Rafael Ribeiro <rafaelribeiro2004@icloud.com>
2025-05-22 11:11:04 -0300

    Initial Commit

---
+++ UniRentAPP/UniRentAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniRentAPPApp.swift
+//  UniRentAPP
+//
+//  Created by Rafael Ribeiro on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct UniRentAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
