fc29b56e424158cd2381e7422d79d6cf17700477
Rafael Ribeiro <125091942+idekrafa@users.noreply.github.com>
2025-05-24 13:10:50 -0300

    Update ContentView.swift

--- UniRentAPP/ContentView.swift
+++ UniRentAPP/ContentView.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedIndex: Int = 0
-    private let categories = ["All", "Apartment", "House", "Studio", "Private Room", "Shared Room"]
     var body: some View {
         VStack {
-            HomeScreen()
+            ZStack {
+                Color("Bg")
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    Image("apt1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .edgesIgnoringSafeArea(.top)
+                    
+                    DescriptionView()
+                        .offset(y: -40)
+                }
+            }
         }
         
     }
@@ -22,194 +33,51 @@ struct ContentView: View {
     ContentView()
 }
 
-struct AppBarView: View {
-    var body: some View {
-        HStack{
-            Button(action: {}) {
-                Image("menu")
-                    .padding()
-                    .background(Color(.white))
-                    .cornerRadius(10.0)
-            }
-            
-            Spacer()
-            
-            Image("Profile")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 50, height: 50)
-                .clipShape(Circle())
-                .overlay(Circle().stroke(Color.white, lineWidth: 2))
-                .shadow(radius: 2)
-        }
-        .padding(.horizontal)
-    }
-}
 
-struct TagLineView: View {
-    var body: some View {
-        Text("Find The \nBest")
-            .font(.custom("PlayfairDisplay-Regular", size: 28))
-            .foregroundColor(Color("Primary"))
-        + Text("Housing!")
-            .font(.custom("PlayfairDisplay-Bold", size: 28))
-            .foregroundColor(Color("Primary"))
-    }
-}
 
-struct SearchAndScanView: View {
-    @State private var search: String = ""
-    var body: some View {
-        HStack {
-            HStack {
-                Image("Search")
-                    .padding(.trailing, 8)
-                TextField("Search Apartments", text: $search)
-            }
-            .padding()
-            .background(Color.white)
-            .cornerRadius(10.0)
-            .padding(.horizontal)
-        }
-    }
-}
 
-struct CategoryView: View {
-    let isActive: Bool
-    let text: String
+struct DescriptionView: View {
     var body: some View {
-        VStack{
-            Text(text)
-                .font(.system(size: 18))
-                .fontWeight(.medium)
-                .foregroundColor(isActive ? Color("Primary"): Color.black.opacity(0.5))
-            
-            if (isActive) {
-                Color("Primary")
-                    .frame(width: 15, height: 2)
-                    .clipShape(Capsule())
-            }
-        }
-        .padding(.trailing)
-    }
-}
-
-struct PropertyCardView: View {
-    let image: Image
-    let size: CGFloat
-    var body: some View {
-        VStack {
-            image
-                .resizable()
-                .frame(width: size, height: 200 * (size/210))
-                .cornerRadius(20.0)
-            Text("Private Room Downtown")
-                .font(.title3)
+        VStack (alignment: .leading) {
+            Text("Private Room \nDowntown")
+                .font(.title)
                 .fontWeight(.bold)
             
-            HStack {
-                Text("1 Bed")
-                
+            HStack (spacing: 4){
+                Text("Affordability")
+                    .padding(.horizontal)
+                ForEach(0 ..< 4) { item in
+                    Image("star")
+                }
                 Spacer()
-                
-                Text("$900")
             }
-        }
-        .frame(width: size)
-        .padding()
-        .background(Color.white)
-        .cornerRadius(20.0)
-    }
-}
-
-struct BottomNavBarItem<Content: View>: View {
-    let content: Content
-    let action: () -> Void
-
-    var body: some View {
-        Button(action: action, label: {
-            content
-                .frame(maxWidth: .infinity)
-        })
-    }
-}
-
-struct HomeScreen: View {
-    var body: some View {
-        ZStack {
-            Color("Bg")
-                .edgesIgnoringSafeArea(.all)
-            ScrollView {
-                VStack (alignment: .leading){
-                    AppBarView()
-                    
-                    TagLineView()
-                        .padding()
-                    
-                    SearchAndScanView()
-                    
-                    ScrollView (.horizontal, showsIndicators: false) {
-                        HStack {
-                            ForEach(0 ..< categories.count) { i in
-                                CategoryView(isActive: i == selectedIndex, text: categories[i])
-                                    .onTapGesture {
-                                        selectedIndex = i
-                                    }
-                            }
-                        }
-                        .padding()
-                    }
-                    
-                    Text("Popular")
-                        .font(.custom("PlayfairDisplay-Bold", size: 24))
-                        .padding(.horizontal)
-                    
-                    ScrollView (.horizontal, showsIndicators: false) {
-                        HStack{
-                            ForEach(0 ..< 4) { index in
-                                PropertyCardView(image: Image("apt\(index + 1)"), size: 210)
-                            }
-                            .padding(.trailing)
-                        }
-                        .padding(.leading)
-                    }
-                    
-                    Text("For you")
-                        .font(.custom("PlayfairDisplay-Bold", size: 24))
-                        .padding(.horizontal)
-                        .padding(.top)
-                    
-                    ScrollView (.horizontal, showsIndicators: false) {
-                        HStack{
-                            ForEach(0 ..< 4) { index in
-                                PropertyCardView(image: Image("apt\(index + 1)"), size: 180)
-                            }
-                            .padding(.trailing)
-                        }
-                        .padding(.leading)
-                    }
+            
+            Text("Description")
+                .fontWeight(.medium)
+                .padding(.vertical, 8)
+            
+            Text("Enjoy ultimate convenience in this bright and quiet private room, located right next to a metro station in the heart of downtown Vancouver.")
+                .lineSpacing(5.0)
+                .opacity(0.8)
+            
+            HStack {
+                VStack (alignment: .leading) {
+                    Text("Info")
+                        .fontWeight(.semibold)
+                        .padding(.bottom, 4)
+                    Text("Beds: 1")
+                        .opacity(0.8)
+                    Text("Furniture: Yes")
+                        .opacity(0.8)
+                    Text("Lease: 6 Months")
+                        .opacity(0.8)
                 }
             }
-            
-            //             custom bottom nav bar
-            
-            HStack {
-                BottomNavBarItem(content: Image("Home")) {}
-                BottomNavBarItem(
-                    content: Image(systemName: "heart")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 24, height: 24)
-                        .foregroundColor(.black)
-                ) {}
-                BottomNavBarItem(content: Image("User")) {}
-            }
-            .padding()
-            .background(Color.white)
-            .clipShape(Capsule())
-            .padding(.horizontal)
-            .shadow(color: Color.black.opacity(0.15), radius: 8, x: 2, y: 6)
-            .frame(maxHeight: .infinity, alignment: .bottom)
+            .padding(.vertical)
         }
+        .padding()
+        .padding(.top)
+        .background(Color("Bg"))
+        .cornerRadius(40.0)
     }
 }
