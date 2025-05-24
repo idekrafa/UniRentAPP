76563997bfbafec8556eb3c04db4a294ce77fdf3
Rafael Ribeiro <125091942+idekrafa@users.noreply.github.com>
2025-05-24 13:11:14 -0300

    Update HomeScreen.swift

--- UniRentAPP/Screens/HomeScreen.swift
+++ UniRentAPP/Screens/HomeScreen.swift
@@ -8,11 +8,200 @@
 import SwiftUI
 
 struct HomeScreen: View {
+    @State private var selectedIndex: Int = 0
+    private let categories = ["All", "Apartment", "House", "Studio", "Private Room", "Shared Room"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("Bg")
+                .edgesIgnoringSafeArea(.all)
+            ScrollView {
+                VStack (alignment: .leading){
+                    AppBarView()
+                    
+                    TagLineView()
+                        .padding()
+                    
+                    SearchAndScanView()
+                    
+                    ScrollView (.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(0 ..< categories.count) { i in
+                                CategoryView(isActive: i == selectedIndex, text: categories[i])
+                                    .onTapGesture {
+                                        selectedIndex = i
+                                    }
+                            }
+                        }
+                        .padding()
+                    }
+                    
+                    Text("Popular")
+                        .font(.custom("PlayfairDisplay-Bold", size: 24))
+                        .padding(.horizontal)
+                    
+                    ScrollView (.horizontal, showsIndicators: false) {
+                        HStack{
+                            ForEach(0 ..< 4) { index in
+                                PropertyCardView(image: Image("apt\(index + 1)"), size: 210)
+                            }
+                            .padding(.trailing)
+                        }
+                        .padding(.leading)
+                    }
+                    
+                    Text("For you")
+                        .font(.custom("PlayfairDisplay-Bold", size: 24))
+                        .padding(.horizontal)
+                        .padding(.top)
+                    
+                    ScrollView (.horizontal, showsIndicators: false) {
+                        HStack{
+                            ForEach(0 ..< 4) { index in
+                                PropertyCardView(image: Image("apt\(index + 1)"), size: 180)
+                            }
+                            .padding(.trailing)
+                        }
+                        .padding(.leading)
+                    }
+                }
+            }
+            
+            //             custom bottom nav bar
+            
+            HStack {
+                BottomNavBarItem(content: Image("Home")) {}
+                BottomNavBarItem(
+                    content: Image(systemName: "heart")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(.black)
+                ) {}
+                BottomNavBarItem(content: Image("User")) {}
+            }
+            .padding()
+            .background(Color.white)
+            .clipShape(Capsule())
+            .padding(.horizontal)
+            .shadow(color: Color.black.opacity(0.15), radius: 8, x: 2, y: 6)
+            .frame(maxHeight: .infinity, alignment: .bottom)
+        }
     }
 }
 
+
 #Preview {
     HomeScreen()
+}
+
+struct AppBarView: View {
+    var body: some View {
+        HStack{
+            Button(action: {}) {
+                Image("menu")
+                    .padding()
+                    .background(Color(.white))
+                    .cornerRadius(10.0)
+            }
+            
+            Spacer()
+            
+            Image("Profile")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 50, height: 50)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                .shadow(radius: 2)
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct TagLineView: View {
+    var body: some View {
+        Text("Find The \nBest")
+            .font(.custom("PlayfairDisplay-Regular", size: 28))
+            .foregroundColor(Color("Primary"))
+        + Text("Housing!")
+            .font(.custom("PlayfairDisplay-Bold", size: 28))
+            .foregroundColor(Color("Primary"))
+    }
+}
+
+struct SearchAndScanView: View {
+    @State private var search: String = ""
+    var body: some View {
+        HStack {
+            HStack {
+                Image("Search")
+                    .padding(.trailing, 8)
+                TextField("Search Apartments", text: $search)
+            }
+            .padding()
+            .background(Color.white)
+            .cornerRadius(10.0)
+            .padding(.horizontal)
+        }
+    }
+}
+
+struct CategoryView: View {
+    let isActive: Bool
+    let text: String
+    var body: some View {
+        VStack{
+            Text(text)
+                .font(.system(size: 18))
+                .fontWeight(.medium)
+                .foregroundColor(isActive ? Color("Primary"): Color.black.opacity(0.5))
+            
+            if (isActive) {
+                Color("Primary")
+                    .frame(width: 15, height: 2)
+                    .clipShape(Capsule())
+            }
+        }
+        .padding(.trailing)
+    }
+}
+
+struct PropertyCardView: View {
+    let image: Image
+    let size: CGFloat
+    var body: some View {
+        VStack {
+            image
+                .resizable()
+                .frame(width: size, height: 200 * (size/210))
+                .cornerRadius(20.0)
+            Text("Private Room Downtown")
+                .font(.title3)
+                .fontWeight(.bold)
+            
+            HStack {
+                Text("1 Bed")
+                
+                Spacer()
+                
+                Text("$900")
+            }
+        }
+        .frame(width: size)
+        .padding()
+        .background(Color.white)
+        .cornerRadius(20.0)
+    }
+}
+
+struct BottomNavBarItem<Content: View>: View {
+    let content: Content
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: action, label: {
+            content
+                .frame(maxWidth: .infinity)
+        })
+    }
 }
